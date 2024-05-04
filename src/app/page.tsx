a9ef741e19bf03a84b45de393d6e2a52f0db76e4
Gurukiran-5c <guru.kiran@5cnetwork.com>
2024-05-04 18:48:37 +0530

    fix:UI updated for the loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,14 +111,14 @@ useEffect(()=>{ handleSave()},[])
       {/* ------saving ui ----------- */}
       <>
       {savingStatus === 'saving' && (
-        <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white px-4 py-2 rounded-full flex items-center">
-          <div className="w-5 h-5 border-4 border-white border-t-transparent rounded-full animate-spin mr-2"></div>
+        <div className="fixed bottom-1 right-1 transform -translate-x-1/2 bg-white text-gray-700 px-1 py-1 flex items-center">
+          <div className="w-5 h-5 border-4 border-gray-700 border-t-transparent rounded-full animate-spin mr-2"></div>
           <span className="mr-2">Saving...</span>
         </div>
       )}
       {savingStatus === 'saved' && (
-        <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white px-4 py-2 rounded-full flex items-center">
-          <svg className="w-5 h-5 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <div className="fixed bottom-1 right-1 transform -translate-x-1/2 bg-white text-gray-700 px-1 py-1 flex items-center">
+          <svg className="w-5 h-5 text-blue-500 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path
               fillRule="evenodd"
               d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
