20d3d31bea204679010b4f37945663b8d6309140
Gurukiran-5c <guru.kiran@5cnetwork.com>
2024-05-06 13:16:41 +0530

    fix: saving ui fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ const handleSave = () => {
     setTimeout(() => {
       setSavingStatus('');
     }, 3000);
-  }, 2000);
+  }, 3000);
 };
 
 
@@ -111,14 +111,14 @@ useEffect(()=>{ handleSave()},[])
       {/* ------saving ui ----------- */}
       <>
       {savingStatus === 'saving' && (
-        <div className="fixed bottom-1 right-1 transform -translate-x-1/2 bg-white text-gray-700 px-1 py-1 flex items-center">
+        <div className="fixed bottom-0 right-1 bg-white text-gray-700 px-1 py-1 flex items-center">
           <div className="w-5 h-5 border-4 border-gray-700 border-t-transparent rounded-full animate-spin mr-2"></div>
-          <span className="mr-2">Saving...</span>
+          <span>Saving...</span>
         </div>
       )}
       {savingStatus === 'saved' && (
-        <div className="fixed bottom-1 right-1 transform -translate-x-1/2 bg-white text-gray-700 px-1 py-1 flex items-center">
-          <svg className="w-5 h-5 text-blue-500 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <div className="fixed bottom-0 right-1 bg-white text-gray-700 px-1 py-1 flex items-center">
+          <svg className="w-5 h-5 text-blue-500 mr-1" fill="green" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path
               fillRule="evenodd"
               d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
